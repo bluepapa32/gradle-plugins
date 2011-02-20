51addc93460c087e59b243311207a8699ed9cf2f
bluepapa32 <bluepapa32.dev@gmail.com>
2011-02-20 23:48:40 +0900

    add native2ascii plugin

---
+++ plugins/native2ascii/src/main/groovy/com/bluepapa32/gradle/plugins/Native2AsciiPlugin.groovy
@@ -0,0 +1,25 @@
+package com.bluepapa32.gradle.plugins
+
+import org.apache.tools.ant.filters.EscapeUnicode
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaBasePlugin
+
+public class Native2AsciiPlugin implements Plugin<Project> {
+    def void apply(Project project) {
+        project.getPlugins().apply(JavaBasePlugin.class);
+
+        project.sourceSets.each { s ->
+            project.tasks.getByName(s.processResourcesTaskName) { task ->
+                task.from(s.resources.srcDirs) {
+                    include '**/*.properties'
+                    filter(EscapeUnicode)
+                }
+                task.from(s.resources.srcDirs) {
+                    exclude '**/*.properties'
+                }
+            }
+        }
+    }
+}
+
