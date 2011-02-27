472fd677f3d0759dc3668c4045075e11e7304bc3
bluepapa32 <bluepapa32.dev@gmail.com>
2011-02-28 00:05:17 +0900

    Added plugin to use git in gradle

---
+++ git/src/main/groovy/com/bluepapa32/gradle/plugins/git/GitPlugin.groovy
@@ -0,0 +1,10 @@
+package com.bluepapa32.gradle.plugins.git
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class GitPlugin implements Plugin<Project> {
+    def void apply(Project project) {
+    }
+}
+
